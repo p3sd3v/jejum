42eb4e829b4bce8f1c7f5b0d2bdfa3f38a267505
Lukerson Boone Pereira <lukerson.www@gmail.com>
2025-06-14 04:47:29 +0000

    ainda ta dando 404 para mim

---
+++ next.config.js
@@ -0,0 +1,33 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/dicas',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+};
+
+export default nextConfig;
